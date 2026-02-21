5fd0416606690d1289ae3949af96d9f8707e58af
Nurlan <abaslinurlan@gmail.com>
2026-02-21 14:50:40 +0400

    Added AddMedia Function

---
+++ MatchMaker/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using MatchMaker.Application.Command.UserCommands;
+using MatchMaker.Application.Services.User;
+using MatchMaker.Shared.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchMaker.Controllers
+{
+    [ApiController]
+    [Route("api/profile")]
+    [Authorize]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _userProfileService;
+
+        public ProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpPost("me/media")]
+        public async Task<IActionResult> AddMedia(AddUserMediaCommand command)
+        {
+            var userId = User.GetUserId();
+            await _userProfileService.AddMediaAsync(userId, command);
+            return Ok();
+        }
+    }
+}
