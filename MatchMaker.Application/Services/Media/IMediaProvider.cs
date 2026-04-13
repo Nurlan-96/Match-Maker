c61151b560bbf2ec84e648ae7e046f5c93013d0c
Nurlan-96 <abaslinurlan@gmail.com>
2026-04-13 21:35:47 +0400

    Created Media Prodvider interface

---
+++ MatchMaker.Application/Services/Media/IMediaProvider.cs
@@ -0,0 +1,13 @@
+﻿using MatchMaker.Application.DTOs;
+using MatchMaker.Domain.Enums;
+
+namespace MatchMaker.Application.Services.Media
+{
+    public interface IMediaProvider
+    {
+        bool CanHandle(MediaCategory category);
+
+        Task<List<MediaSearchResult>> SearchAsync(string query, MediaCategory category);
+        Task<MediaSearchResult?> GetByExternalIdAsync(string externalId, MediaCategory category);
+    }
+}
---
+++ MatchMaker.Application/Services/Media/IMediaProviderFactory.cs
@@ -0,0 +1,9 @@
+﻿using MatchMaker.Domain.Enums;
+
+namespace MatchMaker.Application.Services.Media
+{
+    public interface IMediaProviderFactory
+    {
+        IMediaProvider GetProvider(MediaCategory category);
+    }
+}
