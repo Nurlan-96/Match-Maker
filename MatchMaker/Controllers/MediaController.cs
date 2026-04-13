29e3acd012f601d78f1017ae928ae328596c1268
Nurlan-96 <abaslinurlan@gmail.com>
2026-04-13 21:20:01 +0400

    Created Media Controller

---
+++ MatchMaker/Controllers/MediaController.cs
@@ -0,0 +1,31 @@
+﻿using MatchMaker.Application.DTOs;
+using MatchMaker.Application.Services.Media;
+using MatchMaker.Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchMaker.Controllers
+{
+    [ApiController]
+    [Route("api/media")]
+    public class MediaController : ControllerBase
+    {
+        private readonly IMediaProviderFactory _providerFactory;
+
+        public MediaController(IMediaProviderFactory providerFactory)
+        {
+            _providerFactory = providerFactory;
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<List<MediaSearchResult>>> Search(
+            [FromQuery] string query,
+            [FromQuery] MediaCategory category)
+        {
+            var provider = _providerFactory.GetProvider(category);
+
+            var results = await provider.SearchAsync(query, category);
+
+            return Ok(results);
+        }
+    }
+}
