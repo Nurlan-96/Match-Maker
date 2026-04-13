cdac44770f481f94779561d9e9139111446dab73
Nurlan-96 <abaslinurlan@gmail.com>
2026-04-13 21:37:03 +0400

    changed ints to Guids

--- MatchMaker.Domain/Entities/UserMediaRank.cs
+++ MatchMaker.Domain/Entities/UserMediaRank.cs
@@ -3,9 +3,9 @@ namespace MatchMaker.Domain.Entities
 {
     public class UserMediaRank
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public int MediaId { get; set; }
+        public Guid MediaId { get; set; }
         public MediaEntity Media { get; set; } = null!;
         public int Rank { get; set; }
     }
