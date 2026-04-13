8c05d7d3f2bb3afbc7d5a2e9b4de664a728353ef
Nurlan-96 <abaslinurlan@gmail.com>
2026-04-13 21:23:22 +0400

    Added Media Details and Media Search Results DTOs

---
+++ MatchMaker.Application/DTOs/MediaDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchMaker.Application.DTOs
+{
+    public class MediaDetails
+    {
+        public string ExternalId { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public int? Year { get; set; }
+    }
+}
---
+++ MatchMaker.Application/DTOs/MediaSearchResult.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchMaker.Application.DTOs
+{
+    public class MediaSearchResult
+    {
+        public string ExternalId { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public string? Year { get; set; }
+        public string? ImdbUrl { get; set; }
+    }
+}
