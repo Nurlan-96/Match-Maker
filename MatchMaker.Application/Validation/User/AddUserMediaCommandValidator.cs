f667b9c9591bee6977f97d304f3a7397ea16bc55
Nurlan-96 <abaslinurlan@gmail.com>
2026-04-13 21:36:06 +0400

    removed title

--- MatchMaker.Application/Validation/User/AddUserMediaCommandValidator.cs
+++ MatchMaker.Application/Validation/User/AddUserMediaCommandValidator.cs
@@ -4,14 +4,11 @@ using MatchMaker.Application.Command.UserCommands;
 namespace MatchMaker.Application.Validation.User
 {
     public class AddUserMediaCommandValidator
-    : AbstractValidator<AddUserMediaCommand>
+     : AbstractValidator<AddUserMediaCommand>
     {
         public AddUserMediaCommandValidator()
         {
             RuleFor(x => x.ExternalMediaId)
-                .NotEmpty();
-
-            RuleFor(x => x.Title)
                 .NotEmpty();
 
             RuleFor(x => x.Rank)
